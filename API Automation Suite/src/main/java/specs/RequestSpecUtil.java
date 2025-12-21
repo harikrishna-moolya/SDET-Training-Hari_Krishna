896ea24cd3d3937f15c78f474b6f9201cf78fc04
harikrishna-moolya <hari.krishna@moolya.com>
2025-12-21 22:42:08 +0530

    Update RequestSpecUtil.java

--- API Automation Suite/src/main/java/specs/RequestSpecUtil.java
+++ API Automation Suite/src/main/java/specs/RequestSpecUtil.java
@@ -2,15 +2,20 @@ package specs;
 
 import config.ConfigReader;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.*;
 
 public class RequestSpecUtil {
 
     public static RequestSpecification getRequestSpec() {
+
         return new RequestSpecBuilder()
                 .setBaseUri(ConfigReader.get("base.uri"))
-                .setContentType(ContentType.JSON)
+                .setContentType("application/json")
+                .setAuth(preemptive().basic(
+                        ConfigReader.get("username"),
+                        ConfigReader.get("password")))
                 .log(io.restassured.filter.log.LogDetail.ALL)
                 .build();
     }
