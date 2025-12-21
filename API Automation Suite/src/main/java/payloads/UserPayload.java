8e95c479f32dcdb6953b43129bde910bab510f93
harikrishna-moolya <hari.krishna@moolya.com>
2025-12-21 22:41:41 +0530

    Update UserPayload.java

--- API Automation Suite/src/main/java/payloads/UserPayload.java
+++ API Automation Suite/src/main/java/payloads/UserPayload.java
@@ -2,16 +2,33 @@ package payloads;
 
 public class UserPayload {
 
-    public static String createUserPayload() {
+    public static String createUser(String username) {
         return "{\n" +
                 "  \"id\": 101,\n" +
-                "  \"username\": \"testuser\",\n" +
-                "  \"firstName\": \"Hari\",\n" +
-                "  \"lastName\": \"Krishna\",\n" +
-                "  \"email\": \"hari@test.com\",\n" +
-                "  \"password\": \"password123\",\n" +
+                "  \"username\": \"" + username + "\",\n" +
+                "  \"firstName\": \"Test\",\n" +
+                "  \"lastName\": \"User\",\n" +
+                "  \"email\": \"Hari01@gmail.com\",\n" +
+                "  \"password\": \"HK@123\",\n" +
                 "  \"phone\": \"9999999999\",\n" +
                 "  \"userStatus\": 1\n" +
                 "}";
+    }
+
+    public static String updateUser(String username) {
+        return "{\n" +
+                "  \"id\": 101,\n" +
+                "  \"username\": \"" + username + "\",\n" +
+                "  \"firstName\": \"Updated\",\n" +
+                "  \"lastName\": \"User\",\n" +
+                "  \"email\": \"HK1432@gmail.com\",\n" +
+                "  \"password\": \"HK1432\",\n" +
+                "  \"phone\": \"8888888888\",\n" +
+                "  \"userStatus\": 1\n" +
+                "}";
+    }
+
+    public static String emptyPayload() {
+        return "{}";
     }
 }
