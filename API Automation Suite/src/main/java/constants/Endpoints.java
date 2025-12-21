a47a18aee57bf677b1eb523ad59b23e0bb8699fb
harikrishna-moolya <hari.krishna@moolya.com>
2025-12-21 17:26:59 +0530

    Add files via upload

---
+++ API Automation Suite/src/main/java/constants/Endpoints.java
@@ -0,0 +1,9 @@
+package constants;
+
+public class Endpoints {
+
+    public static final String CREATE_USER = "/v2/user";
+    public static final String GET_USER = "/v2/user/{username}";
+    public static final String UPDATE_USER = "/v2/user/{username}";
+    public static final String DELETE_USER = "/v2/user/{username}";
+}
