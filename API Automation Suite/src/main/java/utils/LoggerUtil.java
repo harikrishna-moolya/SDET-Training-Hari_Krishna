46df84e9a6de3a7d87d056dcf4bd76995d9bfbb6
harikrishna-moolya <hari.krishna@moolya.com>
2025-12-21 22:43:50 +0530

    Update LoggerUtil.java

--- API Automation Suite/src/main/java/utils/LoggerUtil.java
+++ API Automation Suite/src/main/java/utils/LoggerUtil.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class LoggerUtil {
-    public static Logger getLogger(Class<?> cls) {
-        return LogManager.getLogger(cls);
-    }
+
+    public static final Logger log =
+            LogManager.getLogger(LoggerUtil.class);
 }
