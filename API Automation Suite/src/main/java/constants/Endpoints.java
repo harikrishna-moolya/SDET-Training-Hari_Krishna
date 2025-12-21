1999efa8449a579ed330e61643a78716f643d473
harikrishna-moolya <hari.krishna@moolya.com>
2025-12-21 22:41:06 +0530

    Update Endpoints.java

--- API Automation Suite/src/main/java/constants/Endpoints.java
+++ API Automation Suite/src/main/java/constants/Endpoints.java
@@ -2,8 +2,6 @@ package constants;
 
 public class Endpoints {
 
-    public static final String CREATE_USER = "/v2/user";
-    public static final String GET_USER = "/v2/user/{username}";
-    public static final String UPDATE_USER = "/v2/user/{username}";
-    public static final String DELETE_USER = "/v2/user/{username}";
+    public static final String USER = "/user";
+    public static final String USER_BY_NAME = "/user/{username}";
 }
