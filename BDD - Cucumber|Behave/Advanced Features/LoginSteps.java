722bc8b9fdd220a367ab1f26c98c4f4184131d46
harikrishna-moolya <hari.krishna@moolya.com>
2026-01-19 10:07:05 +0530

    Update LoginSteps.java

--- BDD - Cucumber|Behave/Advanced Features/LoginSteps.java
+++ BDD - Cucumber|Behave/Advanced Features/LoginSteps.java
@@ -1,7 +1,5 @@
 package stepdefinitions;
 
-
-import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -10,8 +8,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.time.Duration;
-import java.util.List;
-import java.util.Map;
 
 public class LoginSteps {
 
@@ -24,6 +20,7 @@ public class LoginSteps {
         );
     }
 
+    // ---------- BASIC VALID LOGIN ----------
     @When("the user enters valid username and password")
     public void the_user_enters_valid_username_and_password() {
         Hooks.driver.findElement(By.id("input-email"))
@@ -32,58 +29,49 @@ public class LoginSteps {
                 .sendKeys("1432");
     }
 
-//    @When("the user enters username {string} and password {string}")
-//    public void the_user_enters_username_and_password(String username, String password) {
-//        Hooks.driver.findElement(By.id("input-email")).clear();
-//        Hooks.driver.findElement(By.id("input-email")).sendKeys(username);
-//
-//        Hooks.driver.findElement(By.id("input-password")).clear();
-//        Hooks.driver.findElement(By.id("input-password")).sendKeys(password);
-//    }
+    // ---------- SCENARIO OUTLINE (DATA-DRIVEN) ----------
+    @When("the user enters username {string} and password {string}")
+    public void the_user_enters_username_and_password(String username, String password) {
 
-    @When("the user enters login details")
-    public void the_user_enters_login_details_with_clearing_fields(DataTable dataTable) {
-        List<Map<String, String>> users = dataTable.asMaps(String.class, String.class);
+        WebElement email = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("input-email")));
+        email.clear();
+        email.sendKeys(username);
 
-        for (Map<String, String> user : users) {
-            // Wait and fill email
-            WebElement email = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input-email")));
-            email.clear();
-            email.sendKeys(user.get("username"));
-
-            // Wait and fill password
-            WebElement password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input-password")));
-            password.clear();
-            password.sendKeys(user.get("password"));
-
-            // Wait and click login button
-            WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@value='Login']")));
-            loginButton.click();
-
-            // Validate login success
-            try {
-                wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Logout")));
-                System.out.println("Login successful for: " + user.get("username"));
-            } catch (Exception e) {
-                System.out.println("Login failed for: " + user.get("username"));
-            }
-
-            // Navigate back to login page for next iteration
-            Hooks.driver.get("https://tutorialsninja.com/demo/index.php?route=account/login");
-        }
+        WebElement passwordField = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("input-password")));
+        passwordField.clear();
+        passwordField.sendKeys(password);
     }
-
-
-
-
 
     @When("clicks on the login button")
     public void clicks_on_the_login_button() {
-        Hooks.driver.findElement(By.xpath("//input[@value='Login']")).click();
+        WebElement loginBtn = wait.until(
+                ExpectedConditions.elementToBeClickable(By.xpath("//input[@value='Login']")));
+        loginBtn.click();
     }
 
+    // ---------- SUCCESS VALIDATION ----------
     @Then("the user should be redirected to the dashboard")
     public void the_user_should_be_redirected_to_the_dashboard() {
-       System.out.println("Redirected to Dashboard");
+        WebElement logout = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.linkText("Logout")));
+        Assert.assertTrue(logout.isDisplayed(), "Dashboard not displayed");
+    }
+
+    // ---------- GENERIC RESULT VALIDATION ----------
+    @Then("{string} should be displayed")
+    public void login_result_should_be_displayed(String loginResult) {
+
+        if (loginResult.equalsIgnoreCase("success")) {
+            WebElement logout = wait.until(
+                    ExpectedConditions.visibilityOfElementLocated(By.linkText("Logout")));
+            Assert.assertTrue(logout.isDisplayed(), "Login should be successful");
+        } else {
+            WebElement error = wait.until(
+                    ExpectedConditions.visibilityOfElementLocated(
+                            By.cssSelector(".alert-danger")));
+            Assert.assertTrue(error.isDisplayed(), "Error message should be displayed");
+        }
     }
 }
