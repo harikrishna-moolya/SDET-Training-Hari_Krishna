1830add1decb6648bcf0f1bac31574ce5b11ed25
harikrishna-moolya <hari.krishna@moolya.com>
2026-01-20 14:52:59 +0530

    Update FrameworkException.java

--- API Automation Suite/src/main/java/exception/FrameworkException.java
+++ API Automation Suite/src/main/java/exception/FrameworkException.java
@@ -1,1 +1,12 @@
+package exception;
 
+public class FrameworkException extends RuntimeException {
+
+    public FrameworkException(String message) {
+        super(message);
+    }
+
+    public FrameworkException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
